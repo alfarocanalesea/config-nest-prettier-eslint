395415673d6bcd63e463bb41971e345295b799ff
alfarocanalesea <alf.can.es.al@gmail.com>
2025-01-11 08:58:40 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
         semi: true,
         trailingComma: 'all',
         singleQuote: true,
-        printWidth: 80,
+        printWidth: 200,
         tabWidth: 2,
       },
     ],
